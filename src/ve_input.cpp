f997e676cd0121b13f7f000af9a5e0542626465d
Sean Siddens <seansiddens@gmail.com>
2022-01-23 12:18:38 -0800

    Initial commit

---
+++ src/ve_input.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sean on 1/23/22.
+//
+
+#include "ve_input.h"
---
+++ src/ve_input.h
@@ -0,0 +1,10 @@
+//
+// Created by sean on 1/23/22.
+//
+
+#ifndef VEENGINE_VE_INPUT_H
+#define VEENGINE_VE_INPUT_H
+
+class ve_input {};
+
+#endif  // VEENGINE_VE_INPUT_H
